702494c1713f637bff3a4138fe836353f6e4e411
Kappaed <nathansing.ns@gmail.com>
2021-10-10 21:12:17 +1100

    added redux persist to persist cart data in global redux store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import store from "./store/index";
+import store, { persistedStore } from "./store/index";
 import { Provider } from "react-redux";
 import ScrollIntoView from "./components/shared/ScrollIntoView";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ScrollIntoView>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistedStore}>
+            <App />
+          </PersistGate>
         </Provider>
       </ScrollIntoView>
     </BrowserRouter>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import ProductReducer from "./ProductSlice";
 import CartReducer from "./CartSlice";
+import { persistStore } from "redux-persist";
 
 const combinedReducer = combineReducers({ ProductReducer, CartReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-const store = configureStore({ reducer: combinedReducer });
+const persistedReducer = persistReducer(persistConfig, combinedReducer);
+
+const store = configureStore({ reducer: persistedReducer });
+
+export const persistedStore = persistStore(store);
 export default store;
